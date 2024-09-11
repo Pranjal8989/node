2fac938d34bdac30d951bea38c98dc17cac11e35
pranjal <pranjal.pandit900@gmail.com>
2024-09-11 16:27:36 +0530

    feat: Commitlint Setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends : ["@commitlint", "@commitlint/config-conventional"],
+    extends : ["@commitlint/cli", "@commitlint/config-conventional"],
     rules : {
 "type-enum" :[
     2,
