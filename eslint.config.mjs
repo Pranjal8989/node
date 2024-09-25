ceb15fca87948607f5db91ce82c5e1d07a5f91a8
pranjal <pranjal.pandit900@gmail.com>
2024-09-25 22:39:57 +0530

    feat: Prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
 
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-
+import  eslintConfigPrettier from "eslint-config-prettier"
 export default tseslint.config({
     languageOptions: {
         parserOptions: {
@@ -11,7 +11,7 @@ export default tseslint.config({
         }
     },
     files: ['**/*.ts'],
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked,eslintConfigPrettier],
     rules: {
         'no-console': 'error',
         quotes: ['error', 'single', { allowTemplateLiterals: true }]
