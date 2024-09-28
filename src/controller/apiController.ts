d29cb71b8789a45ba0845551ccab2458d1c47a08
pranjal <pranjal.pandit900@gmail.com>
2024-09-28 13:26:46 +0530

    feat: Crud

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -43,13 +43,26 @@ httpResponse(req,res,200,responseMessage.USERS_FETCHED,userData)
         try {
             const id = req.params.id
             const userExist = await userModel.findOne({ _id: id })
-           
             if (!userExist) {
                 return httpError(next, responseMessage.NOT_FOUND, req, 404)
             }
-            // const userData = await userModel.findByIdAndUpdate(id, req.body, { new: true })
-            // httpResponse(req, res, 200, responseMessage.USER_UPDATED, userData)
-        } catch (err) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            const userData = await userModel.findByIdAndUpdate(id, req.body, { new: true })
+            httpResponse(req, res, 200, responseMessage.USER_UPDATED, userData)
+        } catch (err) { 
+            httpError(next, err, req, 500)
+        }
+    },
+    deleteUser: async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const id = req.params.id
+            const userExist = await userModel.findOne({ _id: id })
+            if (!userExist) {
+                return httpError(next, responseMessage.NOT_FOUND, req, 404)
+            }
+            const userData = await userModel.findByIdAndDelete({_id:id})
+            httpResponse(req, res, 200, responseMessage.USER_DELETED, userData)
+        } catch (err) { 
             httpError(next, err, req, 500)
         }
     },
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import health from './health'
 import userRoute from './apiRouter'
+import loginRoute from './loginRoute'
 const router = express.Router()
 const defaultRoutes = [
     {
@@ -10,6 +11,10 @@ const defaultRoutes = [
     {
         path:'/user',
         route:userRoute
+    },
+    {
+        path:'/login',
+        route:loginRoute
     }
 
 ]
